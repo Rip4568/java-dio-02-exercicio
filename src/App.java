aced9fda3bff09fcccb6dc1e6ae948fe4346c50f
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-25 18:48:55 -0300

    desafio pratico 3 process oseletivo feito

--- src/App.java
+++ src/App.java
@@ -1,20 +1,33 @@
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
+    private static ArrayList<String> candidatosSelecionados = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         /* DESAFIO 2 */
         String [] candidatos = {"A", "B","C", "D", "E", "F", "G"};
-        Integer candidatosSelecionados = 0;
+        Integer candidatosSelecionadosQuantidade = 0;
         for (String candidato : candidatos) {
             if (analisarCandidato(salarioPretendido())) {
                 System.out.println("Candidato : " + candidato + " Selecionado");
-                candidatosSelecionados++;
-                if (candidatosSelecionados > 4) {
+                selecionarCandidato(candidato);
+                candidatosSelecionadosQuantidade++;
+                if (candidatosSelecionadosQuantidade > 4) {
                     break;
                 }
             }
         }
 
+    }
+
+    public static void imprimirCandidatosSelecionados() {
+        for (int index = 0; index < candidatosSelecionados.size(); index++) {
+            System.out.println("Candidato N°: " + index + " selecionado, nome: " + candidatosSelecionados.get(index));
+        }
+    }
+
+    static void selecionarCandidato(String candidato) {
+        candidatosSelecionados.add(candidato);
     }
 
     public static Double salarioPretendido() {
@@ -24,13 +37,13 @@ public class App {
     public static Boolean analisarCandidato(Double salarioPretendido) {
         final Double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
-            System.out.println("LIGAR PARA O CANDIDATO");
+            /* System.out.println("LIGAR PARA O CANDIDATO"); */
             return true;
         } else if (salarioBase == salarioPretendido) {
-            System.out.println("AGENDAR CANDIDATO");
+            /* System.out.println("AGENDAR CANDIDATO"); */
             return true;
         } else {
-            System.out.println("ANALISAR OUTROS CANDIDATOS");
+            /* System.out.println("ANALISAR OUTROS CANDIDATOS"); */
             return false;
         }
     }
