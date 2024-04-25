a6c953a2b953792b7b9abca51d246c5492cf0b74
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-25 17:54:37 -0300

    desafio pratico 2 process oseletivo feito

--- src/App.java
+++ src/App.java
@@ -1,20 +1,37 @@
-import interfaces.OptionsType;
-import utils.BetterInput;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Double salarioPretendido = (Double) BetterInput.input("Digite o seu salario pretendido: ", OptionsType.Double);
-        analisarCandidato(salarioPretendido);
+        /* DESAFIO 2 */
+        String [] candidatos = {"A", "B","C", "D", "E", "F", "G"};
+        Integer candidatosSelecionados = 0;
+        for (String candidato : candidatos) {
+            if (analisarCandidato(salarioPretendido())) {
+                System.out.println("Candidato : " + candidato + " Selecionado");
+                candidatosSelecionados++;
+                if (candidatosSelecionados > 4) {
+                    break;
+                }
+            }
+        }
+
     }
 
-    static void analisarCandidato(Double salarioPretendido) {
+    public static Double salarioPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    public static Boolean analisarCandidato(Double salarioPretendido) {
         final Double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO");
+            return true;
         } else if (salarioBase == salarioPretendido) {
             System.out.println("AGENDAR CANDIDATO");
+            return true;
         } else {
             System.out.println("ANALISAR OUTROS CANDIDATOS");
+            return false;
         }
     }
 }
